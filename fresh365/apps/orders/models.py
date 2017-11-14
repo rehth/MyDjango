e8465e42f95addbcf2b1b52e53ad17e917b276f5
张千军 <1515171493@qq.com>
2017-11-14 17:08:48 +0800

    建立订单模型类的相关表

--- fresh365/apps/orders/models.py
+++ fresh365/apps/orders/models.py
@@ -1,3 +1,48 @@
 from django.db import models
-
+from db.base_model import BaseModel
 # Create your models here.
+
+
+class OrderInfo(BaseModel):
+    ''' 订单信息模型类 '''
+    pay_method_choices = [
+        (1, '货到付款'),
+        (2, '微信支付'),
+        (3, '支付宝'),
+        (4, '银联支付'),
+    ]
+    order_status_choices = [
+        (1, '未支付'),
+        (2, '待发货'),
+        (3, '待收货'),
+        (4, '待评价'),
+        (5, '已完成'),
+    ]
+    order_id = models.CharField(max_length=128, primary_key=True, verbose_name='订单id')
+    user = models.ForeignKey('user.User', verbose_name='用户')
+    addr = models.ForeignKey('user.Address', verbose_name='地址')
+    pay_method = models.SmallIntegerField(default=3, choices=pay_method_choices, verbose_name='付款方式')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='总价')
+    total_count = models.IntegerField(default=1, verbose_name='数量')
+    transit_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='运费')
+    order_status = models.SmallIntegerField(default=1, verbose_name='订单状态', choices=order_status_choices)
+    pay_id = models.CharField(max_length=128, verbose_name='支付编号')
+
+    class Meta:
+        db_table = 'order_info'
+        verbose_name = '订单信息'
+        verbose_name_plural = verbose_name
+
+
+class OrderGoods(BaseModel):
+    ''' 订单商品模型类 '''
+    order = models.ForeignKey('OrderInfo', verbose_name='订单')
+    count = models.IntegerField(default=1, verbose_name='商品数目')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='商品价格')
+    comment = models.CharField(max_length=256, verbose_name='评论')
+    sku = models.ForeignKey()
+
+    class Meta:
+        db_table = 'order_goods'
+        verbose_name = '订单商品'
+        verbose_name_plural = verbose_name
--- fresh365/apps/user/models.py
+++ fresh365/apps/user/models.py
@@ -8,7 +8,21 @@ class User(AbstractUser, BaseModel):
     ''' 用户表的模型类 '''
 
     class Meta:
-        db_table = 'fresh_user'
+        db_table = 'user'
         verbose_name = '用户'
         verbose_name_plural = verbose_name
 
+
+class Address(BaseModel):
+    '''　地址表的模型类　'''
+    user = models.ForeignKey('User', verbose_name='所属用户')
+    receiver = models.CharField(max_length=20, verbose_name='收件人')
+    addr = models.CharField(max_length=256, verbose_name='收件地址')
+    zip_code = models.CharField(max_length=6, verbose_name='邮政编码')
+    phone = models.CharField(max_length=11, verbose_name='手机')
+    is_default = models.BooleanField(default=False, verbose_name='是否默认')
+
+    class Meta:
+        db_table = 'address'
+        verbose_name = '地址'
+        verbose_name_plural = verbose_name
