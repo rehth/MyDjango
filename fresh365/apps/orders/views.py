8de7a39dde6abab9521812184b9ff8ddc6ab44d1
retht <1405507731@qq.com>
2017-11-24 18:06:44 +0800

    mysql事务优化及测试悲观锁使用以控制并发

--- fresh365/apps/orders/views.py
+++ fresh365/apps/orders/views.py
@@ -64,11 +64,13 @@ class OrderPlace(LoginRequiredMixin):
             'trans_price': trans_price, 'total_pay': total_pay,
             'addrs': addrs, 'total_price': total_price, 'sku_ids': sku_ids
         }
-        return render(request, 'test.html', context)
+        return render(request, 'place_order.html', context)
 
 
 # ajax post请求 url:/orders/commit
 # 参数：sku_ids, addr_id, pay_method
+# mysql的事务实现
+# 悲观锁测试
 class OrdersCommit(LoginRequiredMixin):
     """订单提交"""
     def get(self, request):
@@ -109,47 +111,56 @@ class OrdersCommit(LoginRequiredMixin):
         # todo:业务处理-添加一个订单信息表, n个订单商品表
         # 订单信息表
         # 订单id:秒级时间戳+user.id
-        order_id = datetime.datetime.now().strftime('%Y%m%d%H%M%S')+str(user.id)
-        transit_price = 10
-        orders = OrderInfo.objects.create(order_id=order_id, user=user,
-                                          addr=addr, pay_method=pay_method,
-                                          total_count=total_count, total_price=total_price,
-                                          transit_price=transit_price, pay_id='Null')
-        # 购物车查询
-        conn = get_redis_connection('default')
-        cart_key = 'cart_%s' % user.id
-        for sku_id in sku_ids:
-            try:
-                sku = GoodsSKU.objects.get(id=sku_id)
-            except GoodsSKU.DoesNotExist:
-                transaction.savepoint_rollback(sid)
-                return JsonResponse({'res': 3, 'msg': '商品不存在或已下架'})
-            # 商品数量
-            sku.count = conn.hget(cart_key, sku_id)    # b'4'
-            # 商品库存检验
-            if int(sku.count) > int(sku.stock):
-                transaction.savepoint_rollback(sid)
-                return JsonResponse({'res': 4, 'msg': '库存数量不足'})
-            total_count += int(sku.count)
-            # 商品小计
-            amount = int(sku.count) * float(sku.price)
-            sku.amount = float('%.2f' % amount)
-            total_price += sku.amount
-            orders_sku.append(sku)
-        # 订单商品表
-        for sku in orders_sku:
-            OrderGoods.objects.create(order=orders, sku=sku,
-                                      count=sku.count, price=sku.price,
-                                      comment='Null')
-            # 购物车数据更新
-            conn.hdel(cart_key, sku.id)
-            # order_info信息更新
-            orders.total_count = total_count
-            orders.total_price = total_price
-            orders.save()
-            # 商品库存更新
-            sku.stock -= int(sku.count)
-            sku.save()
+        try:
+            order_id = datetime.datetime.now().strftime('%Y%m%d%H%M%S')+str(user.id)
+            transit_price = 10
+            orders = OrderInfo.objects.create(order_id=order_id, user=user,
+                                              addr=addr, pay_method=pay_method,
+                                              total_count=total_count, total_price=total_price,
+                                              transit_price=transit_price, pay_id='Null')
+            # 购物车查询
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%s' % user.id
+            # import time
+            # time.sleep(10)
+            for sku_id in sku_ids:
+                try:
+                    # 悲观锁测试  select_for_update()
+                    sku = GoodsSKU.objects.select_for_update().get(id=sku_id)
+                except GoodsSKU.DoesNotExist:
+                    transaction.savepoint_rollback(sid)
+                    return JsonResponse({'res': 3, 'msg': '商品不存在或已下架'})
+                # 商品数量
+                sku.count = conn.hget(cart_key, sku_id)    # b'4'
+                # 商品库存检验
+                if int(sku.count) > int(sku.stock):
+                    transaction.savepoint_rollback(sid)
+                    return JsonResponse({'res': 4, 'msg': '库存数量不足'})
+                total_count += int(sku.count)
+                # 商品小计
+                amount = int(sku.count) * float(sku.price)
+                sku.amount = float('%.2f' % amount)
+                total_price += sku.amount
+                orders_sku.append(sku)
+            # 订单商品表
+            for sku in orders_sku:
+                OrderGoods.objects.create(order=orders, sku=sku,
+                                          count=sku.count, price=sku.price,
+                                          comment='Null')
+                # 购物车数据更新
+                conn.hdel(cart_key, sku.id)
+                # order_info信息更新
+                orders.total_count = total_count
+                orders.total_price = total_price
+                orders.save()
+                # 商品库存更新
+                sku.stock -= int(sku.count)
+                sku.save()
+        except Exception as e:
+            transaction.savepoint_rollback(sid)
+            return JsonResponse({'res': 4, 'msg': e})
+        # todo:事务提交
+        transaction.savepoint_commit(sid)
 
         # todo:返回应答
         return JsonResponse(data={'res': 5, 'msg': '提交成功'})
