82d80626c749cc4f229a4b98eaf2608bdca1207c
retht <1405507731@qq.com>
2017-11-24 17:16:36 +0800

    给订单提交功能添加事务：回滚机制

--- fresh365/apps/orders/views.py
+++ fresh365/apps/orders/views.py
@@ -8,6 +8,7 @@ from django_redis import get_redis_connection
 from apps.orders.models import OrderInfo, OrderGoods
 import datetime
 from django.http import JsonResponse
+from django.db import transaction
 
 # Create your views here.
 
@@ -73,6 +74,7 @@ class OrdersCommit(LoginRequiredMixin):
     def get(self, request):
         return redirect(reverse('cart:info'))
 
+    @transaction.atomic
     def post(self, request):
         # todo:数据接受
         user = request.user
@@ -89,38 +91,21 @@ class OrdersCommit(LoginRequiredMixin):
         total_price = 0
         if not all([sku_ids, addr_id, pay_method]):
             return JsonResponse({'res': 0, 'msg': '信息不完整'})
-        # 购物车查询
-        conn = get_redis_connection('default')
-        cart_key = 'cart_%s' % user.id
-        for sku_id in sku_ids:
-
-            try:
-                sku = GoodsSKU.objects.get(id=sku_id)
-            except GoodsSKU.DoesNotExist:
-                return JsonResponse({'res': 1, 'msg': '商品不存在或已下架'})
-            # 商品数量
-            sku.count = conn.hget(cart_key, sku_id)    # b'4'
-
-            # 商品库存检验
-            if int(sku.count) > int(sku.stock):
-                return JsonResponse({'res': 2, 'msg': '库存数量不足'})
-            total_count += int(sku.count)
-            # 商品小计
-            amount = int(sku.count) * float(sku.price)
-            sku.amount = float('%.2f' % amount)
-            total_price += sku.amount
-            orders_sku.append(sku)
         # pay_method
         for pay in OrderInfo.pay_method_choices:
             if int(pay_method) in pay:
                 break
         else:
-            return JsonResponse({'res': 3, 'msg': '支付方式有误'})
+            return JsonResponse({'res': 1, 'msg': '支付方式有误'})
         # 收货地址
         try:
             addr = Address.objects.get(user=user, id=addr_id)
         except Address.DoesNotExist:
-            return JsonResponse({'res': 4, 'msg': '收货地址有误'})
+            return JsonResponse({'res': 2, 'msg': '收货地址有误'})
+
+        # todo: 设置一个保存点
+        sid = transaction.savepoint()
+
         # todo:业务处理-添加一个订单信息表, n个订单商品表
         # 订单信息表
         # 订单id:秒级时间戳+user.id
@@ -130,6 +115,27 @@ class OrdersCommit(LoginRequiredMixin):
                                           addr=addr, pay_method=pay_method,
                                           total_count=total_count, total_price=total_price,
                                           transit_price=transit_price, pay_id='Null')
+        # 购物车查询
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%s' % user.id
+        for sku_id in sku_ids:
+            try:
+                sku = GoodsSKU.objects.get(id=sku_id)
+            except GoodsSKU.DoesNotExist:
+                transaction.savepoint_rollback(sid)
+                return JsonResponse({'res': 3, 'msg': '商品不存在或已下架'})
+            # 商品数量
+            sku.count = conn.hget(cart_key, sku_id)    # b'4'
+            # 商品库存检验
+            if int(sku.count) > int(sku.stock):
+                transaction.savepoint_rollback(sid)
+                return JsonResponse({'res': 4, 'msg': '库存数量不足'})
+            total_count += int(sku.count)
+            # 商品小计
+            amount = int(sku.count) * float(sku.price)
+            sku.amount = float('%.2f' % amount)
+            total_price += sku.amount
+            orders_sku.append(sku)
         # 订单商品表
         for sku in orders_sku:
             OrderGoods.objects.create(order=orders, sku=sku,
@@ -137,8 +143,13 @@ class OrdersCommit(LoginRequiredMixin):
                                       comment='Null')
             # 购物车数据更新
             conn.hdel(cart_key, sku.id)
+            # order_info信息更新
+            orders.total_count = total_count
+            orders.total_price = total_price
+            orders.save()
             # 商品库存更新
             sku.stock -= int(sku.count)
+            sku.save()
 
         # todo:返回应答
         return JsonResponse(data={'res': 5, 'msg': '提交成功'})
