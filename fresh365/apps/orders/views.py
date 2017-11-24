85da4a66490e24e1c0311472e8ecf664c1c11854
retht <1405507731@qq.com>
2017-11-24 22:22:17 +0800

    订单提交之乐观锁实现(注:要修改mysql数据库的隔离级别

--- fresh365/apps/orders/views.py
+++ fresh365/apps/orders/views.py
@@ -52,8 +52,8 @@ class OrderPlace(LoginRequiredMixin):
         # 运费
         trans_price = 10
         # 实际支付
-        total_price = float('%.2f' % total_price)
-        total_pay = total_price + trans_price
+        total_price = ('%.2f' % total_price)
+        total_pay = ('%.2f' % (float(total_price) + trans_price))
 
         # 收货地址
         addrs = Address.objects.filter(user=user)
@@ -71,7 +71,7 @@ class OrderPlace(LoginRequiredMixin):
 # 参数：sku_ids, addr_id, pay_method
 # mysql的事务实现
 # 悲观锁测试
-class OrdersCommit(LoginRequiredMixin):
+class OrdersCommit1(LoginRequiredMixin):
     """订单提交"""
     def get(self, request):
         return redirect(reverse('cart:info'))
@@ -155,6 +155,8 @@ class OrdersCommit(LoginRequiredMixin):
                 orders.save()
                 # 商品库存更新
                 sku.stock -= int(sku.count)
+                # 商品销量
+                sku.sales += int(sku.count)
                 sku.save()
         except Exception as e:
             transaction.savepoint_rollback(sid)
@@ -166,3 +168,107 @@ class OrdersCommit(LoginRequiredMixin):
         return JsonResponse(data={'res': 5, 'msg': '提交成功'})
 
 
+# 乐观锁测试
+class OrdersCommit(LoginRequiredMixin):
+    """订单提交"""
+    def get(self, request):
+        return redirect(reverse('cart:info'))
+
+    @transaction.atomic
+    def post(self, request):
+        # todo:数据接受
+        user = request.user
+        sku_ids = request.POST.get('sku_ids')
+        addr_id = request.POST.get('addr_id')
+        pay_method = request.POST.get('pay_method')
+        sku_ids = sku_ids.split(',')
+        # todo:数据检验
+        if not all([sku_ids, addr_id, pay_method]):
+            return JsonResponse({'res': 0, 'msg': '信息不完整'})
+        # pay_method
+        for pay in OrderInfo.pay_method_choices:
+            if int(pay_method) in pay:
+                break
+        else:
+            return JsonResponse({'res': 1, 'msg': '支付方式有误'})
+        # 收货地址
+        try:
+            addr = Address.objects.get(user=user, id=addr_id)
+        except Address.DoesNotExist:
+            return JsonResponse({'res': 2, 'msg': '收货地址有误'})
+        # 商品总数量
+        total_count = 0
+        # 商品总价格
+        total_price = 0
+        # 购物车查询
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%s' % user.id
+
+        # todo: 设置一个保存点
+        sid = transaction.savepoint()
+
+        # todo:业务处理-添加一个订单信息表, n个订单商品表
+        # 订单信息表
+        # 订单id:秒级时间戳+user.id
+        try:
+            order_id = datetime.datetime.now().strftime('%Y%m%d%H%M%S')+str(user.id)
+            transit_price = 10
+            orders = OrderInfo.objects.create(order_id=order_id, user=user,
+                                              addr=addr, pay_method=pay_method,
+                                              total_count=total_count, total_price=total_price,
+                                              transit_price=transit_price, pay_id='Null')
+            for sku_id in sku_ids:
+                for i in range(3):
+                    try:
+                        sku = GoodsSKU.objects.get(id=sku_id)
+                    except GoodsSKU.DoesNotExist:
+                        transaction.savepoint_rollback(sid)
+                        return JsonResponse({'res': 3, 'msg': '商品不存在或已下架'})
+                    # 商品数量
+                    sku.count = conn.hget(cart_key, sku_id)  # b'4'
+                    # 商品库存检验
+                    if int(sku.count) > sku.stock:
+                        transaction.savepoint_rollback(sid)
+                        return JsonResponse({'res': 4, 'msg': '库存数量不足'})
+                    # 更新前数据记录
+                    original_stock = sku.stock
+                    print('user:%s, stock:%s' % (user.username, original_stock))
+                    new_stock = int(original_stock) - int(sku.count)
+                    new_sales = int(sku.sales) + int(sku.count)
+
+                    total_count += int(sku.count)
+                    # 商品小计
+                    amount = int(sku.count) * float(sku.price)
+                    sku.amount = float('%.2f' % amount)
+                    total_price += sku.amount
+
+                    # 订单商品表
+                    OrderGoods.objects.create(order=orders, sku=sku,
+                                              count=sku.count, price=sku.price,
+                                              comment='Null')
+                    # 商品库存/商品销量更新
+                    # 返回受影响的行数row
+                    res = GoodsSKU.objects.filter(id=sku_id, stock=original_stock).\
+                        update(stock=new_stock, sales=new_sales)
+                    if not res:
+                        # 更新失败
+                        if i == 2:
+                            return JsonResponse({'res': 7, 'msg': '订单提交失败'})
+                        continue
+                        
+                    # order_info信息更新
+                    orders.total_count = total_count
+                    orders.total_price = total_price
+                    orders.save()
+                    # 购物车数据更新
+                    conn.hdel(cart_key, sku.id)
+                    break
+
+        except Exception as e:
+            transaction.savepoint_rollback(sid)
+            return JsonResponse({'res': 6, 'msg': e})
+        # todo:事务提交
+        transaction.savepoint_commit(sid)
+
+        # todo:返回应答
+        return JsonResponse(data={'res': 5, 'msg': '提交成功'})
